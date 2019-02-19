5aa09f3bdd3113a28afa9a0e9cfcc4a9d1aec127
Moises Abarca <moisesabarca@gmail.com>
2019-02-19 17:40:12 -0300

    first commit

---
+++ src/main/java/com/org/learningMaven/capacitacionAndres/App.java
@@ -0,0 +1,13 @@
+package com.org.learningMaven.capacitacionAndres;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
